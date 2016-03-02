4ef49ebad703f001060c3705d053fa5a2e262fae
Phillip Alday <phillip.alday@unisa.edu.au>
2016-03-02 16:00:55 +1030

    more tinkering with plots

--- Makefile
+++ Makefile
@@ -4,6 +4,13 @@
 %-handout.pdf: %.md
 	pandoc -o $* $<
 
+cluster.pdf scatter.pdf: intro-to-mvpa.py
+	python intro-to-mvpa.py
+
+intro-to-mvpa-slides.pdf: intro-to-mvpa.md cluster.pdf scatter.pdf
+	pandoc -o $@ $< --to="beamer" --slide-level=2 --include-in-header="surface.tex" --latex-engine=xelatex
+ 
+# intro-to-eep-erp-slides.pdf
 %-slides.pdf: %.md
 	pandoc -o $@ $< --to="beamer" --slide-level=2 --include-in-header="surface.tex" --latex-engine=xelatex
 	
--- intro-to-mvpa.py
+++ intro-to-mvpa.py
@@ -8,10 +8,10 @@ from matplotlib import pyplot as plt
 sns.set(style="white",palette="muted",color_codes=True)
 
 blobs = datasets.make_blobs(n_samples=100,
-							n_features=2,
-							centers=[(-2,2),(2,-2)],
-							cluster_std=[1.3,1.5],
-							random_state=42)
+                            n_features=2,
+                            centers=[(-2,2),(2,-2)],
+                            cluster_std=[1.3,1.5],
+                            random_state=42)
 df = pd.DataFrame(blobs[0],columns=["x","y"])
 df["group"] = blobs[1]
 df["group_color"] = [sns.categorical.color_palette("muted")[_] for _ in blobs[1]]
@@ -22,3 +22,20 @@ scatter.savefig("scatter.pdf")
 cluster = sns.jointplot(x="x",y="y",data=df, kind="kde")
 cluster.plot_joint(plt.scatter, c="w", s=30, linewidth=1, marker="+")
 cluster.savefig("cluster.pdf")
+
+def distplot_color(a,color=None,**kwargs):
+    if "ax" in kwargs:
+        ax = kwargs["ax"]
+        del kwargs["ax"]
+    else:
+        ax = None
+    for c in color.unique():
+        selection = (color == c)
+        ax = sns.distplot(a[selection],color=c,ax=ax,**kwargs)
+    
+    return ax
+
+color_scatter = sns.JointGrid(x="x",y="y",data=df)
+color_scatter  = color_scatter.plot_joint(plt.scatter,color=df.group_color)
+color_scatter  = color_scatter.plot_marginals(distplot_color,color=df.group_color)
+color_scatter; plt.show()
